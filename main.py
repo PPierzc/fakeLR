c4c2dce1ab557e2bca39a8e60804356ef0b82145
Paweł <ppierzc@gmail.com>
2020-11-19 21:43:26 +0100

    change model

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
         data = json.loads(event['body'])
         if 'input' in data.keys():
             input_data = int(data['input'])
-            prediction = 3 * input_data + 1
+            prediction = 2 * input_data + 1
             return prediction
     else:
         return 'error'
