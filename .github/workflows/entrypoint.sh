92dc78715b53806597f3fc3ebc1d965d4075d137
Paweł <ppierzc@gmail.com>
2020-11-20 17:54:40 +0100

    pylambda action.yml

---
+++ .github/workflows/entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+install_zip_dependencies(){
+	echo "Installing and zipping dependencies..."
+	mkdir python
+	curl https://files.pythonhosted.org/packages/f5/bf/4981bcbee43934f0adb8f764a1e70ab0ee5a448f6505bd04a87a2fda2a8b/numpy-1.16.1-cp36-cp36m-manylinux1_x86_64.whl > numpy.whl
+	unzip numpy.whl
+	rm -r *.whl *.dist-info
+	mv numpy ./python
+	pip install --target=python -r "${INPUT_REQUIREMENTS_TXT}"
+	zip -r dependencies.zip ./python
+}
+
+publish_dependencies_as_layer(){
+	echo "Publishing dependencies as a layer..."
+	local result=$(aws lambda publish-layer-version --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --zip-file fileb://dependencies.zip)
+	LAYER_VERSION=$(jq '.Version' <<< "$result")
+	rm -rf python
+	rm dependencies.zip
+}
+
+publish_function_code(){
+	echo "Deploying the code itself..."
+	zip -r code.zip . -x \*.git\*
+	aws lambda update-function-code --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --zip-file fileb://code.zip
+}
+
+update_function_layers(){
+	echo "Using the layer in the function..."
+	aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers "${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}"
+}
+
+deploy_lambda_function(){
+	install_zip_dependencies
+	publish_dependencies_as_layer
+	publish_function_code
+	update_function_layers
+}
+
+deploy_lambda_function
+echo "Done."
