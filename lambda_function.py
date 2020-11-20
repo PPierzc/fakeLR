62fcc8445c14efbc88d110241c6a5d1dc8a2619a
Paweł <ppierzc@gmail.com>
2020-11-20 10:52:28 +0100

    change model add dependency

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,11 @@
 import json
+import numpy as np
 
 def lambda_handler(event, context):
     if 'body' in event.keys():
         data = json.loads(event['body'])
         if 'input' in data.keys():
-            input_data = int(data['input'])
+            input_data = np.float(data['input'])
             prediction = 5 * input_data + 1
             return prediction
     else:
